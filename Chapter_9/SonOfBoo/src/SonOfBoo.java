f348f3dfcbcf732f6c10da503458921dc817e63b
AlexanderSwift89 <alexander.lyapunov89@gmail.com>
2020-08-04 23:51:30 +0300

    The commit of 2020-08-04

---
+++ Chapter_9/SonOfBoo/src/SonOfBoo.java
@@ -0,0 +1,29 @@
+public class SonOfBoo extends Boo {
+    //number 1
+    public SonOfBoo() {
+        super("boo");
+    }
+    //number 2
+    public SonOfBoo(int i) {
+        super("Fred");
+    }
+    //number 3
+    public SonOfBoo(String s) {
+        super(42);
+    }
+    //number 4
+    public SonOfBoo(int i, String s) {
+    }
+    //number 5
+    public SonOfBoo(String a, String b, String c) {
+        super(a,b);
+    }
+    //number 6
+    public SonOfBoo(int i, int j) {
+        super("man",j);
+    }
+    //number 7
+    public SonOfBoo(int i, int x, int y) {
+        super(i, "star");
+    }
+}
---
+++ Chapter_9/SonOfBoo/src/testSonOfBoo.java
@@ -0,0 +1,6 @@
+public class testSonOfBoo {
+    public static void main(String[]args) {
+        //SonOfBoo oneSon = new SonOfBoo();
+        SonOfBoo oneSon = new SonOfBoo(42);
+    }
+}
